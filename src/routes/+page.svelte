18248a974eaf23c9cbb7451dca2f5b5985cd25fc
ahammednibras8 <ahammednibras@proton.me>
2024-12-04 21:41:42 +0530

    Added Name and Role

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,12 @@
 			<div class="h-48 w-48 overflow-hidden rounded-full bg-[#161513] p-1">
 				<img src="/assets/profile.jpeg" alt="profile" class="h-full w-full object-cover" />
 			</div>
+            
+            <!-- Name and Role -->
+            <h1 class="text-4xl font-bold text-white mb-2">Ahammed Nibras</h1>
+            <h2 class="text-2xl font-semibold bg-gradient-to-r from-rose-400 to-blue-500 bg-clip-text text-transparent">Frontend Developer</h2>
+
+            <!-- Description -->
 		</div>
 	</div>
 </div>
